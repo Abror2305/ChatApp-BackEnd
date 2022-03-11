74f0e80183dfb527a74bb30870dddee53b61d6fa
Abror Alisherov <74832377+Abror2305@users.noreply.github.com>
2022-03-11 22:27:49 +0500

    register completed

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
+'use strict'
 const FakeExpress = require("./src/lib")
 const PORT = process.env.PORT || 4000
 
 const app = FakeExpress()
 const login = require("./src/controllers/login")
-
+const register = require("./src/controllers/register")
 app.post("/login", login)
+app.post("/register",register)
 
 app.listen(PORT,() => {
     console.log("Server is running at http://192.168.1.2:"+PORT)
--- src/controllers/register.js
+++ src/controllers/register.js
@@ -1,7 +1,43 @@
 'use strict'
+const {read,hash,write} = require("../util")
 
 module.exports = async (req,res) => {
-    res.setHeader("Access-Control-Allow-Origin", "*")
+    try {
+        res.setHeader("Access-Control-Allow-Origin", "*")
+        let {username, age, password} = await req.body
 
+        let users = read("users") || []
 
+        let user = users.find(user => user.username === username)
+
+        if (user) {
+            throw new Error("This username is already taken")
+        }
+        if (!password || password.length < 6 || password.length > 32) {
+            throw new Error("The password length must be more than 6 and less than 32")
+        }
+        if (isNaN(+age) || age < 12 || age > 100) {
+            throw new Error("Age must be greater than 12 and less than 100")
+        }
+
+        let newUser = {
+            user_id: users.at(-1)?.user_id + 1 || 1,
+            username,
+            age,
+            password
+        }
+        users.push(newUser)
+
+        write("users", users)
+        res.json({
+            status: 200,
+            user_id: hash(newUser.user_id)
+        })
+    }
+    catch (e) {
+        res.json({
+            status:403,
+            message: e.message
+        })
+    }
 }
